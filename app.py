f355f104e753d24fc7ba3e747ac46b16626030b6
Kyrylo Shakirov <kyrylo.shakirov@zorallabs.com>
2019-04-25 18:03:23 +0300

    graph db started

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from bottle import get, post, run, response
+from bottle import get, post, run, response, request
 from json import dumps
 import sys, bottle
+
 sys.path.append('lib/')
 from graph_manager import GraphManager
 
-
-app = bottle.default_app()             # or bottle.Bottle() if you prefer
+app = bottle.default_app()  # or bottle.Bottle() if you prefer
 app.config['gm'] = GraphManager('localhost', 'root', 'servantes')
 
 
@@ -13,15 +13,22 @@ app.config['gm'] = GraphManager('localhost', 'root', 'servantes')
 def index():
     return 200
 
-@app.get('/entity/parent/fullpath')
-def index():
+
+@app.get('/entity/<id>/fullpath')
+def index(id):
     g_manager = app.config.get('gm')
-    path = g_manager.get_all_parents(1)
+    path = g_manager.get_all_parents(id)
     r = [{"path": path}]
     response.content_type = 'application/json'
     return dumps(r)
 
 
+@app.post('/entity/parent/add')
+def index():
+    body = request.json
+    print(body)
+    g_manager = app.config.get('gm')
+    g_manager.add_pair(body)
 
 
 run(host='localhost', port=8081)
--- lib/graph_manager.py
+++ lib/graph_manager.py
@@ -4,41 +4,55 @@ from arango import ArangoClient
 class GraphManager:
     VERTEX_COLLECTION = 'entity'
     EDGE_COLLECTION = 'reference'
+    DB = 'PimsDb'
 
     def __init__(self, hostname, login, password):
         client = ArangoClient(protocol='http', host=hostname, port=8529)
-        self.db = client.db('PimsDb', username=login, password=password)
+        self.db = client.db(self.DB, username=login, password=password)
         self.pimsGraph = self.db.graph('PimsGraph')
 
     def add_entity(self, entity_data):
         entities = self.pimsGraph.vertex_collection(self.VERTEX_COLLECTION)
         entities.insert({'_key': str(entity_data['id']), 'name': entity_data['name']})
 
-    def add_reference(self,parent_id, child_id):
+    def update_entity(self, id, name):
+        entities = self.pimsGraph.vertex_collection(self.VERTEX_COLLECTION)
+        entity = entities.get(str(id))
+        entity['name'] = name
+        entities.update(entity)
+
+    def add_reference(self, parent_id, child_id):
         references = self.pimsGraph.edge_collection(self.EDGE_COLLECTION)
-        edge_data = {'_key': '%(p_id)d_%(ch_id)d'%{'p_id': parent_id, 'ch_id':child_id},
-                                '_from': 'entity/%(p_id)d'%{'p_id': parent_id},'_to': 'entity/%(ch_id)d'%{'ch_id': child_id }}
+        edge_data = {'_key': '%(p_id)d_%(ch_id)d' % {'p_id': parent_id, 'ch_id': child_id},
+                     '_from': 'entity/%(p_id)d' % {'p_id': parent_id}, '_to': 'entity/%(ch_id)d' % {'ch_id': child_id}}
         references.insert(edge_data)
 
-    def add_pair(self,entity_data):
+    def has_reference(self, parent_id, child_id):
+        references = self.pimsGraph.edge_collection(self.EDGE_COLLECTION)
+        key = '%(p_id)d_%(ch_id)d' % {'p_id': parent_id, 'ch_id': child_id}
+        return references.has(key)
+
+    def add_pair(self, entity_data):
         entities = self.pimsGraph.vertex_collection(self.VERTEX_COLLECTION)
         if not entities.has(str(entity_data['id'])):
             self.add_entity(entity_data)
-        elif not entities.has(str(entity_data['reference_id'])):
-            self.add_entity({'id': entity_data['reference_id'],'name': None })
         else:
+            self.update_entity(entity_data['id'], entity_data['name'])
+
+        if not entities.has(str(entity_data['reference_id'])):
+            self.add_entity({'id': entity_data['reference_id'], 'name': (entity_data['parent_name'] or '')})
+        else:
+            self.update_entity(entity_data['reference_id'], entity_data['parent_name'])
+
+        if self.has_reference(entity_data['reference_id'], entity_data['id']):
             print()
-        self.add_reference(entity_data['reference_id'], entity_data['id'])
-    def get_all_parents(self,entity_id):
-        response = self.pimsGraph.traverse(start_vertex='entity/%(id)d'%{'id':entity_id},
-                                direction='inbound', strategy='bfs',
-                                edge_uniqueness='global',
-                                vertex_uniqueness='global')
+        else:
+            self.add_reference(entity_data['reference_id'], entity_data['id'])
 
+    def get_all_parents(self, entity_id):
+        response = self.pimsGraph.traverse(start_vertex='entity/%(id)s' % {'id': entity_id},
+                                           direction='inbound', strategy='bfs',
+                                           edge_uniqueness='global',
+                                           vertex_uniqueness='global')
 
-        return  ':'.join(map(lambda  r: str(r['name']),response['vertices']))
-
-
-
-
-
+        return ':'.join(map(lambda r: str(r['name']), response['vertices']))
